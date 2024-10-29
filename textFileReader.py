a40183967212bf89ecaf543ca37b010da57bc9bb
Goose! <buckminster.hayesmcqueen@yellowwoodacademy.org>
2024-10-29 13:45:36 -0700

    Update textFileReader.py

--- textFileReader.py
+++ textFileReader.py
@@ -15,9 +15,25 @@ def uploadFile():
         with open(f"texts/{file}" , 'r', encoding='UTF-8') as f:
             return f.read()
 
+def exitChoice():
+    choice2 = input("Would you like to leave? (Y/N): ")
+    if str(choice2).lower() == "y":
+        exit()
+    elif str(choice2).lower() == "n":
+        print()
+        fileReader()
 def fileReader():
     fileName = uploadFile()
     fileLength = len(fileName)
     print(f"Your file is {fileLength} characters long.")
+    print()
+    choice = input("Would you like to read it? (Y/N): ")
+    if choice.lower() == "y":
+        print(fileName)
+        print()
+        exitChoice()
+    else:
+        exitChoice()
+        
         
 fileReader()
