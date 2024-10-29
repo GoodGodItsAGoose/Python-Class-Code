11fd217e47901a8376cfbc7132162577ada5f727
Goose! <buckminster.hayesmcqueen@yellowwoodacademy.org>
2024-10-29 13:34:08 -0700

    bleh
    you need the text folder in order for this to work

---
+++ textFileReader.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+def uploadFile():
+    file = input("Please enter the file name you've uploaded: ")
+    if ".txt" in file:
+        print()
+    else:
+        file = file+".txt"
+    try: 
+        open(f"texts\{file}" , 'r', encoding='UTF-8')
+    except:
+        UnicodeDecodeError
+        print("File not found. Please try again.")
+        fileReader()
+    else:
+        with open(f"texts\{file}" , 'r', encoding='UTF-8') as f:
+            return f.read()
+
+def fileReader():
+    fileName = uploadFile()
+    fileLength = len(fileName)
+    print(f"Your file is {fileLength} characters long.")
+        
+fileReader()
