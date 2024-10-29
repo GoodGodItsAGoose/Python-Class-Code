ee3b400e9fbf8dd1e2df1ef1a959d9b4727e9c38
Goose! <buckminster.hayesmcqueen@yellowwoodacademy.org>
2024-10-29 13:48:17 -0700

    Update textFileReader.py

--- textFileReader.py
+++ textFileReader.py
@@ -1,10 +1,10 @@
-from pathlib import Path
 def uploadFile():
     file = input("Please enter the file name you've uploaded: ")
     if ".txt" in file:
         print()
     else:
         file = file+".txt"
+    #checks to see if file name will work
     try: 
         open(f"texts/{file}" , 'r', encoding='UTF-8')
     except:
@@ -12,9 +12,11 @@ def uploadFile():
         print("File not found. Please try again.")
         fileReader()
     else:
+        #opens the file and returns the text
         with open(f"texts/{file}" , 'r', encoding='UTF-8') as f:
             return f.read()
 
+#choice to exit(what the name says it does)
 def exitChoice():
     choice2 = input("Would you like to leave? (Y/N): ")
     if str(choice2).lower() == "y":
@@ -22,6 +24,8 @@ def exitChoice():
     elif str(choice2).lower() == "n":
         print()
         fileReader()
+
+# main function, able to read or upload a new file.
 def fileReader():
     fileName = uploadFile()
     fileLength = len(fileName)
