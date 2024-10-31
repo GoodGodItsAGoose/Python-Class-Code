688990d3eb7253e3b219b2ff9abcf00e1821124a
Goose! <buckminster.hayesmcqueen@yellowwoodacademy.org>
2024-10-31 13:42:48 -0700

    pseudocode

---
+++ PYTHON/newFile.py
@@ -0,0 +1,8 @@
+"""
+psudocode for append method
+"""
+
+# initialize the data and Node
+# have the head become the node if the head is set to None and return it
+# if head is not set to None, it will continually move the data from a 
+# previous node to the next node until there are no more nodes to be moved.
