83a39d27a0c5e77ee38f650f0325e62f85d3e57f
Goose! <buckminster.hayesmcqueen@yellowwoodacademy.org>
2025-01-24 17:06:34 -0800

    Update node.py

--- node.py
+++ node.py
@@ -26,21 +26,6 @@ class linked_list:
         # Old: [Head--> [(anything else)--> [None] ] ] 
         # Inserting: [(new data)--> [Head--> [(anything else)--> [None] ] ] ]
         # New: [Head--> [(new data)--> [(anything else)--> [None] ] ] ]
-        
-    def find_node(self, node_data):
-        cur_node = self.head
-        num = 0
-        while cur_node:
-            if cur_node.data == node_data:
-                return num
-            else:
-                cur_node = cur_node.next
-                num += 1
-                
-    def edit_node(self, num):
-        finder = input("Input node data to edit: ")
-        self.find_node(int(finder))
-        # make a change node function
     
     def insert_at_end(self, data):
         node = Node(data)
@@ -57,6 +42,82 @@ class linked_list:
             node.next = self.head
             self.head = node
     
+    def deleteAtEnd(self):
+        cur = self.head
+        if self.head is None:
+            print("Nothing to delete.")
+        else:
+            while cur.next.next is not None:
+                cur = cur.next
+            cur.next = None
+    
+    def delete_at_beginning(self):
+        node = self.head
+        if node.next is None:
+            self.head = node.next
+        else:
+            self.head = self.head.next
+    
+    # Other functions
+    def find_node(self, node_data):
+        cur_node = self.head
+        num = 0
+        while cur_node:
+            if cur_node.data == node_data:
+                return num
+            else:
+                cur_node = cur_node.next
+                num += 1
+    
+    def find_node_index(self, node_index):
+        cur_node = self.head
+        num = 1
+        try:
+            while num != node_index:
+                cur_node = cur_node.next
+                num += 1
+            return cur_node.data
+        except IndexError or AttributeError:
+            while num != node_index-1:
+                cur_node = cur_node.next
+                num += 1
+            return cur_node.data
+    
+    # def edit_node(self):
+    #     temp = self.head
+    #     finder = input("Input node data to edit: ")
+    #     num = self.find_node(int(finder))
+    #     input_choice = input("Input edited data: ")
+    #     # make a change node function
+    #     listies = []
+    #     while temp:
+    #         listies.append(temp.data)
+    #         temp = temp.next
+    #     temp = self.head
+    #     for _ in range(num-1):
+    #         temp = temp.next
+    #     # maybe insert into list, remake list into linked list
+    #     new_node = Node(input_choice)
+    #     temp.next = new_node # deletes the rest of the linked list
+    #     num1 = 0 + (num - 1)
+    #     for _ in range(len(listies) - (num - 1)):
+    #         self.insert_at_end(listies[num1])
+    #         num1 += 1
+    
+    def edit(self, node_data, changed_node_data):
+        temp = self.head
+        num = self.find_node(node_data)
+
+        for _ in range(int(num)):
+            temp = temp.next
+
+        new_node = Node(changed_node_data)
+        if temp.next is None:
+            new_node.next = temp.next
+        else:
+            new_node.next = temp.next.next
+        temp.data = new_node.data
+    
     def display(self):
         temp = self.head
         # [Temp(Head)-->[(Data)-->[None]]
@@ -65,11 +126,11 @@ class linked_list:
             
             temp = temp.next
             # [(Head)-->[Temp(data)-->[None]]]
-ll = linked_list()
-ll.insert_at_beginning(15)
-ll.insert_at_beginning(25)
-ll.insert_at_beginning(35)
-ll.insert_at_beginning(45)
-ll.insert_at_beginning(55)
-print(ll.find_node(35))
-ll.display()        
+    
+    def length(self):
+        temp = self.head
+        num = 0
+        while temp:
+            temp = temp.next
+            num += 1
+        return num
