306fe73f81ed64e719b223dc03b2e30c01caf852
Goose! <buckminster.hayesmcqueen@yellowwoodacademy.org>
2025-01-28 07:21:10 -0800

    Add files via upload

---
+++ pseudocodeStacks.py
@@ -0,0 +1,13 @@
+# first, initialize the stack.
+
+# The functions:
+#   - Pop
+# Pop takes the top item of the stack and returns and deletes it.
+#   - Push
+# Push adds a new item onto the stack
+#   - Peek
+# Peek returns the top item of the stack
+#   - Is_Empty
+# Is_Empty checks to see if the stack is empty
+#   - Length
+# Length returns the length of the stack
